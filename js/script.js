f71c3e6739850b85a03fd95e6f1e406b9cdf4859
Andrew Price <40673634+AndrewPricee@users.noreply.github.com>
2018-12-08 19:21:07 +0200

    working version

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// Change style of navbar on scroll
+window.onscroll = function () {
+    myFunction();
+};
+
+function myFunction() {
+    var navbar = document.getElementsByClassName("navbar");
+    if (document.documentElement.clientWidth > 767) {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            $(navbar).css('background-color', '#FCFCFC');
+            $(navbar).css('box-shadow', '0 0.25px #FFFFFF');
+            $(".navbar a").css('color', '#666');
+            $(".navbar a").css('transition-duration', '2s');
+            $(navbar).css('transition-duration', '0.3s');
+        } else {
+            $(navbar).css('background-color', 'transparent');
+            $(".navbar a").css('color', '#fff');
+            $(".navbar a").css('transition-duration', '2s');
+            $(navbar).css('box-shadow', 'none');
+            $(navbar).css('transition-duration', '0.3s');
+        }
+    }
+}
+
+//progress bars
+
+var lang = {
+    "html": "90%",
+    "css": "80%",
+    "javascript": "70%",
+    "angular": "65%",
+    "java": "60%",
+    "php": "30%",
+    "wordpress": "60%",
+    "mysql": "70%"
+  };
+  
+  var multiply = 4;
+  
+  $.each( lang, function( language, percent) {
+  
+    var delay = 700;
+    
+    setTimeout(function() {
+      $('#'+language+'-percent').html(percent);
+    },delay*multiply);
+    
+    multiply++;
+  
+  });
+
+  //scrolling
+
+  //Smooth scroll - Used W3schools scroll
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+$('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 650, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
